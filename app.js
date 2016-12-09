02dd9d2c6997562d96325b1d5c7dc2b3c1b0f389
Mike <mikeytown19@gmail.com>
2016-12-08 23:33:15 -0700

    added the border walls for the snake to be constrained

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ function Snake(){
     }
 
     this.update = function(){
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
+        this.x = this.x + this.xspeed*scl;
+        this.y = this.y + this.yspeed*scl;
+        this.x = constrain(this.x, 0, width-scl);
+        this.y = constrain(this.y, 0, height-scl);
     }
 
     this.show = function(){
         fill(255);
-        rect(this.x, this.y, 10, 10)
+        rect(this.x, this.y, scl, scl)
 
     }
 }
